0345e1bd1729c9689a364bec945e2ed108950e48
McCaffreyCarson <carson@coulstontech.net>
2016-10-14 14:58:27 -0400

    Added in some more commands

--- bot.js
+++ bot.js
@@ -1,14 +1,39 @@
 var Discord = require('discord.js');
 var bot = new Discord.Client();
+const token = 'MjAyMjQ4MDM1NzA5ODc4Mjcy.CuKvWw.vvyeOvxAgQCfNDRdylU4m_0V_Dg'
+var quotes = ["I CAN'T HEAR YOU"];
+var quotesLength = quotes.length;
 
 bot.on('ready', () => {
   console.log('I am ready!');
 });
 
 bot.on('message', message => {
-  if (message.content === 'ping') {
+
+  //Replies "pong"
+  if(message.content === 'ping') {
     message.reply('pong');
+  }
+
+  //Returns the User's avatar
+  if(message.content === '!avatar') {
+    message.reply(message.author.avatarURL);
+  }
+
+  //Adds a quote to the quotes array
+  if(message.content.startsWith('!quote add')) {
+    var quotes = spliceArguments(message.content)[1];
+    console.log(quotes);
+  }
+
+  //Returns a random quote from the quotes array
+  if(message.content === '!quote') {
+    var randomQuote = Math.floor(Math.random() * quotesLength);
+    console.log(randomQuote);
+    quoteReply = "" + quotes[randomQuote];
+    console.log(quoteReply);
+    //message.reply(quoteReply);
   }
 });
 
-bot.login('MjAyMjQ4MDM1NzA5ODc4Mjcy.CuKvWw.vvyeOvxAgQCfNDRdylU4m_0V_Dg');
+bot.login(token);
